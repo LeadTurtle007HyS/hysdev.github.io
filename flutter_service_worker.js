65ff0778927fb5254da867a2154316f81a0c3de8
Pratik Ekghare <pratik.e@sparrowrms.in>
2022-01-02 15:39:15 +0530

    update https by http in api

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -369,7 +369,7 @@ const RESOURCES = {
 "assets/packages/flutter_tex/js/katex/fonts/KaTeX_Typewriter-Regular.woff": "b636fa487ef5e2f56bb3529bc9168be0",
 "assets/packages/flutter_tex/js/katex/fonts/KaTeX_Typewriter-Regular.woff2": "8073cf012f453ddf47d5b8776f6bb1e5",
 "assets/packages/flutter_tex/js/katex/index.html": "f0b07d87f116752c2c8de2882f9db878",
-"/": "b49bd43e4d14e44c2e2a88f9479dca4f",
+"/": "91b2ce0c375890d2f7d4f3da7369f19a",
 "assets/packages/flutter_tex/js/katex/katex.css": "cf85eb8b74953ecc5b3685c0969b4349",
 "assets/packages/flutter_tex/js/katex/katex.js": "6a42d26befadf4cadea19a9978c48396",
 "assets/packages/flutter_tex/js/katex/katex.min.css": "f9417c96e906891af6c4153115f0e471",
@@ -526,8 +526,8 @@ const RESOURCES = {
 "icons/Icon-512x512.png": "3382d162605ab3a0adfcf01d873de3f3",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b49bd43e4d14e44c2e2a88f9479dca4f",
-"main.dart.js": "bc84630ff61cab473c549aa3d316c213",
+"index.html": "91b2ce0c375890d2f7d4f3da7369f19a",
+"main.dart.js": "96f263b8a20d0cb5ed17ee024b08588b",
 "manifest.json": "7c1755639acc82966595d21d15d7588c",
 "version.json": "f2f160099d92c95cbf6c6374ae68d701"
 };
